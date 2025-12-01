a63f846f6154b6c6e14d6d422b3f1500e9892147
Alma Capone <bella.vicky2020@gmail.com>
2025-12-01 10:21:13 -0600

    sugar and salt

--- src/Sugar_Salt.java
+++ src/Sugar_Salt.java
@@ -9,6 +9,10 @@ import edu.macalester.graphics.Rectangle;
 
 public class Sugar_Salt implements Stand {
   String name = "Sugar & Salt";
+  double X = size*0.1;
+  double Y= size*0.2;
+  double Width = 40; 
+  double Height =10;
 
   @Override
   public String getName() {
@@ -16,15 +20,7 @@ public class Sugar_Salt implements Stand {
 
   }
 
-  @Override
-  public GraphicsObject setStandProperties(double size){
-    double X = size*0.1;
-    double Y= size*0.2;
-    double Width = 40; 
-    double Height =10;
-    Rectangle saltSugarShape = new Rectangle(X, Y, Width, Height);
-    return saltSugarShape;
-  }
+  
 
   
   @Override
@@ -32,6 +28,38 @@ public class Sugar_Salt implements Stand {
     ingredientsList.add("Sugar");
     ingredientsList.add("Salt");
 
+  }
+
+  @Override
+  public GraphicsObject getGraphics(double size) {
+    Rectangle saltSugarShape = new Rectangle(X, Y, Width, Height);
+    return saltSugarShape;
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getGraphics'");
+  }
+
+  @Override
+  public int getX() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getX'");
+  }
+
+  @Override
+  public int getY() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getY'");
+  }
+
+  @Override
+  public int getWidth() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getWidth'");
+  }
+
+  @Override
+  public int getHeight() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
   }
 
 }
